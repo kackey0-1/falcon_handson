cbb32bd03b323af85e9aebd0dac2b5c8b0fe3a9d
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2021-03-06 00:59:37 +0900

    adding nuxtjs for application build.

---
+++ ui/pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app permanent>
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              Sample App
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <v-list dense class="pt-0">
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <h1 class="header">Nuxt TypeScript Starter</h1>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+import { State } from "vuex-class"
+import type { Person } from "~/types";
+import Card from "~/components/Card.vue"
+
+@Component({
+  components: {
+    Card
+  }
+})
+export default class extends Vue {
+  @State people!: Person[]
+}
+</script>
+
+<style scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
